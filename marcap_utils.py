2f14e3b6841e2d951346f3e33beba8d11433cd61
plusjune <plusjune@financedata.kr>
2019-12-04 15:58:31 +0900

    marcap_date_range() to marcap_data()

--- marcap_utils.py
+++ marcap_utils.py
@@ -4,42 +4,12 @@
 from datetime import datetime
 import numpy as np
 import pandas as pd
-import glob
 
-def marcap_date(date):
-  '''
-  지정한 날짜의 시가총액 순위 데이터
-  :param datetime theday: 날짜
-  :return: DataFrame
-  '''
-  date = pd.to_datetime(date)
-  csv_file = 'marcap/data/marcap-%s.csv.gz' % (date.year)
-  result = None
-  try: 
-    dtypes={'Code':str, 'Name':str, 
-            'Open':float, 'High':float, 'Low':float, 'Close':float, 'Volume':float, 'Amount':float,
-            'Changes':float, 'ChagesRatio':float, 'Marcap':float, 'Stocks':float, 'MarcapRatio':float, 
-            'ForeignShares':float, 'ForeignRatio':float, 'Rank':float}
-
-    df = pd.read_csv(csv_file, dtype=dtypes, parse_dates=['Date'])
-    cols = ['Date', 'Code', 'Name',
-            'Open', 'High', 'Low', 'Close', 'Volume', 'Amount', 
-            'Changes', 'ChagesRatio', 'Marcap', 'Stocks', 'MarcapRatio', 
-            'ForeignShares', 'ForeignRatio', 'Rank']
-    result = df[cols]
-    result = result[result['Date'] == date]
-    result = result.sort_values(['Date','Rank'])
-  except Exception as e:
-    print(e)
-    return None
-  result.set_index('Date', inplace=True)
-  return result[result['Volume'] > 0]
-
-def marcap_date_range(start, end=None, code=None):
+def marcap_data(start, end=None, code=None):
   '''
   지정한 기간 데이터 가져오기
   :param datetime start: 시작일
-  :param datetime end: 종료일
+  :param datetime end: 종료일 (지정하지 않으면 시작일과 동일)
   :param str code: 종목코드 (지정하지 않으면 모든 종목)
   :return: DataFrame
   '''
