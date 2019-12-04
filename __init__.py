6e9af41aed8b56fe0574f88416074d7925620545
FinanceData.KR <plusjune@gmail.com>
2019-12-04 16:39:31 +0900

    Update __init__.py
    update marcap_data

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # marcap_utils.py - 시가총액 데이터를 위한 유틸함수
 
-from .marcap_utils import marcap_date, marcap_date_range
+from .marcap_utils import marcap_data
 
 __version__ = '0.2.0'
 
 __all__ = ['__version__', 
-    'marcap_date',
-    'marcap_date_range',
+    'marcap_data',
 ]
