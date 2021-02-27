459285baaee3058d369a891070a3ce6a0eb2a09e
HOEBIN JEONG <31682226+stat17-hb@users.noreply.github.com>
2021-02-27 10:50:11 +0900

    Update marcap_utils.py
    pd.read_csv에서 int 타입 지정으로 인한 에러 발생. Amount, Marcap의 dtype을 float로 수정

--- marcap_utils.py
+++ marcap_utils.py
@@ -6,36 +6,43 @@ import numpy as np
 import pandas as pd
 
 def marcap_data(start, end=None, code=None):
-  '''
-  지정한 기간 데이터 가져오기
-  :param datetime start: 시작일
-  :param datetime end: 종료일 (지정하지 않으면 시작일과 동일)
-  :param str code: 종목코드 (지정하지 않으면 모든 종목)
-  :return: DataFrame
-  '''
-  start = pd.to_datetime(start)
-  end = start if end==None else pd.to_datetime(end)
-  df_list = []
+    '''
+    지정한 기간 데이터 가져오기
+    :param datetime start: 시작일
+    :param datetime end: 종료일 (지정하지 않으면 시작일과 동일)
+    :param str code: 종목코드 (지정하지 않으면 모든 종목)
+    :return: DataFrame
+    '''
+    start = pd.to_datetime(start)
+    end = start if end==None else pd.to_datetime(end)
+    df_list = []
 
-  dtypes={'Code':str, 'Name':str, 
-          'Open':int, 'High':int, 'Low':int, 'Close':int, 'Volume':int, 'Amount':int,
-          'Changes':int, 'ChangeCode':str, 'ChagesRatio':float, 'Marcap':int, 'Stocks':int,
-          'MarketId':str, 'Market':str, 'Dept':str,
-          'Rank':int}
+#     dtypes={'Code':str, 'Name':str, 
+#             'Open':int, 'High':int, 'Low':int, 'Close':int, 'Volume':int, 'Amount':int,
+#             'Changes':int, 'ChangeCode':str, 'ChagesRatio':float, 'Marcap':int, 'Stocks':int,
+#             'MarketId':str, 'Market':str, 'Dept':str,
+#             'Rank':int}
 
-  for year in range(start.year, end.year + 1):
-    try:
-      csv_file = 'marcap/data/marcap-%s.csv.gz' % (year)
-      df = pd.read_csv(csv_file, dtype=dtypes, parse_dates=['Date'])
-      df_list.append(df)
-    except Exception as e:
-      print(e)
-      pass
-  df_merged = pd.concat(df_list)
-  df_merged = df_merged[(start <= df_merged['Date']) & (df_merged['Date'] <= end)]  
-  df_merged = df_merged.sort_values(['Date','Rank'])
-  if code:
-    df_merged = df_merged[code == df_merged['Code']]  
-  df_merged.set_index('Date', inplace=True)
-  return df_merged[df_merged['Volume'] > 0]
+# Amount, Marcap을 int 타입으로 변환할 수 없다는 에러메시지 발생
 
+    dtypes={'Code':str, 'Name':str, 
+            'Open':int, 'High':int, 'Low':int, 'Close':int, 'Volume':int, 'Amount':float,
+            'Changes':int, 'ChangeCode':str, 'ChagesRatio':float, 'Marcap':float, 'Stocks':int,
+            'MarketId':str, 'Market':str, 'Dept':str,
+            'Rank':int}
+
+    for year in range(start.year, end.year + 1):
+        try:
+            csv_file = 'marcap/data/marcap-%s.csv.gz' % (year)
+            df = pd.read_csv(csv_file, dtype=dtypes, parse_dates=['Date'])
+            df_list.append(df)
+        except Exception as e:
+            print(e)
+            pass
+    df_merged = pd.concat(df_list)
+    df_merged = df_merged[(start <= df_merged['Date']) & (df_merged['Date'] <= end)]  
+    df_merged = df_merged.sort_values(['Date','Rank'])
+    if code:
+        df_merged = df_merged[code == df_merged['Code']]  
+    df_merged.set_index('Date', inplace=True)
+    return df_merged[df_merged['Volume'] > 0]
